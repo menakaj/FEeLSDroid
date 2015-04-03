774696ed570a1465ecc9ce14ad9b5893f950be94
Menaka <menaka12350@gmail.com>
2015-04-03 16:25:45 +0530

    home screen created

---
+++ Documents/Software_Projects/Java/adroid/FEELSDroid/src/com/hackers/FEELSDroid/Home.java
@@ -0,0 +1,16 @@
+package com.hackers.FEELSDroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.hackers.FEELSDroid.R;
+
+/**
+ * Created by menaka on 4/3/15.
+ */
+public class Home extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+    }
+}
